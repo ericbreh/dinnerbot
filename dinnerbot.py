fb486e6e23b759c2b29a6c770983ebf548ec2aa5
Eric <echuang1@ucsc.edu>
2023-01-31 21:02:28 -0800

    headless works

--- dinnerbot.py
+++ dinnerbot.py
@@ -1,28 +1,26 @@
-#dinnerbot account
-username = 'dinnerbot50'
-password = 'python1'
-message_to = 'Eric Chuang'
+username = 'hungry.tamara'
+password = 'yourmomonmonday'
+message_to = 'honey nut cheerios'
 message = 'Dinner at 7'
 
-from playwright.sync_api import sync_playwright, ViewportSize
+from playwright.sync_api import sync_playwright
+import schedule
 import time
 
 def send_message():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.set_viewport_size(ViewportSize(width = 1000*2, height = 1920*2))
         page.goto("https://www.instagram.com/direct/inbox/")
-
         page.get_by_label('username').fill(username)
         page.get_by_label('password').fill(password)
         page.get_by_text('Log in').nth(0).click()
-        page.get_by_text('Not Now').nth(0).click()
-        time.sleep(4)
         page.get_by_text('Not Now').nth(0).click()
         page.get_by_text(message_to).nth(0).click()
         page.get_by_role("textbox").fill(message)
         page.get_by_role("textbox").press('Enter')
 
-
-send_message()
+schedule.every().day.at("15:00").do(send_message)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
---
+++ pw_bot.py
@@ -0,0 +1,31 @@
+#dinnerbot account
+username = 'dinnerbot50'
+password = 'python1'
+message_to = 'Order of the cone'
+message = 'WAKE UP'
+
+from playwright.sync_api import sync_playwright
+import time
+
+def send_message():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://www.instagram.com/direct/inbox/")
+
+        page.get_by_label('username').fill(username)
+        page.get_by_label('password').fill(password)
+        page.get_by_text('Log in').nth(0).click()
+        page.get_by_text('Not Now').nth(0).click()
+        # page.get_by_text('Not Now').nth(0).click()
+        page.get_by_text(message_to).nth(0).click()
+
+        # page.get_by_role("textbox").fill(message)
+        # page.get_by_role("textbox").press('Enter')
+            
+
+        # time.sleep(5)
+        # page.screenshot(path="1.png")
+
+
+send_message()
